338d7b4b9cb518e970f6c56656265341ed9de8c9
ZiquitaRiberdy <riberdyziquita@gmail.com>
2024-03-05 11:34:51 -0800

    Added new file head.js

---
+++ head.js
@@ -0,0 +1,25 @@
+// FUNCTION IMPLEMENTATION
+let firstElement;
+const assertEqual = function(actual, expected) {
+    if (actual === expected) {
+      console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+    } else {
+      console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+    }
+  };
+  
+const head = (elementArray) => {
+    firstElement = elementArray[0];
+    //console.log(firstElement)
+    return firstElement;
+}
+
+//head([5,6,7]);
+  
+// TEST CODE
+assertEqual("Lighthouse Labs", "Bootcamp");
+assertEqual(1, 1);
+assertEqual("Lighthouse Labs", "Lighthouse Labs");
+assertEqual(1, 10);
+assertEqual(head([5,6,7]), 5);
+assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
