845a3034c907c95b80d8aee60fd6b7619a2f3359
ZiquitaRiberdy <riberdyziquita@gmail.com>
2024-03-19 10:45:51 -0700

    new file

---
+++ assertArraysEqual.js
@@ -0,0 +1,20 @@
+const eqArrays = function(arr1, arr2){
+    let answer = true;
+
+    if (arr1.length !== arr2.length){
+        //return false;
+        console.log(`False ${arr1} and ${arr2} are not equal`);
+
+    } else {
+        for (let i = 0; i < arr1.length; i++){
+            if (arr1[i] !== arr2[i]) {
+              console.log(`False ${arr1} and ${arr2} are not equal`);
+              break;
+        }console.log(`True ${arr1} and ${arr2} are equal`);
+    } //console.log(`True ${arr1} and ${arr2} are equal`);
+  }
+}
+
+//TEST
+eqArrays([1, 2, 3], [1, 2, 3])
+//eqArrays([1, 2, 3], [3, 2, 1])
