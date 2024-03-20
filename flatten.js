09c18d45ae89c3cde3f8c2d4f5be0e5bb0698726
ZiquitaRiberdy <riberdyziquita@gmail.com>
2024-03-20 10:00:04 -0700

    fixed issue with text not uploading

---
+++ flatten.js
@@ -0,0 +1,45 @@
+const flatten = function(array){
+    let newArray = [];
+    for(item of array){
+        if (Array.isArray(item)){
+            for (element of item){
+                newArray.push(element)
+            }
+        } else {
+            newArray.push(item);
+        }
+    }
+    console.log(newArray)
+    //return newArray;
+}
+
+const assertArraysEqual = function(ar1, ar2) {
+    //eqArrays(ar1,ar2);
+    if (eqArrays(ar1,ar2)) {
+      console.log(`✅✅✅ Assertion Passed: ${ar1} === ${ar2}`) 
+    } else {
+        console.log(`🛑🛑🛑 Assertion Failed: ${ar1} !== ${ar2}`)
+    }
+  };
+
+const eqArrays = function(arr1, arr2){
+    let answer = true;
+
+    if (arr1.length !== arr2.length){
+        answer = false;
+    } else {
+        //return answer;
+        for (let i = 0; i < arr1.length; i++){
+            if (arr1[i] !== arr2[i]) {
+                answer = false;
+                break;
+            }
+        }
+    }
+    return answer;
+    //assertArraysEqual(answer);
+  }
+
+  //TEST
+  flatten([1, 2, [3, 4], 5, [6]])
+
