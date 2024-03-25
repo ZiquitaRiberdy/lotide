51427ceb04a5a484f25e4953c7e41dc37f96d1c1
ZiquitaRiberdy <riberdyziquita@gmail.com>
2024-03-25 13:16:10 -0700

    added test files to lotide for head and assertequal

--- head.js
+++ head.js
@@ -1,12 +1,5 @@
+const assertEqual = require("./assertEqual");
 // FUNCTION IMPLEMENTATION
-//let firstElement;
-const assertEqual = function(actual, expected) {
-    if (actual === expected) {
-      console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
-    } else {
-      console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
-    }
-  };
   
 const head = (elementArray) => {
     return elementArray[0];
@@ -14,12 +7,5 @@ const head = (elementArray) => {
     //return firstElement;
 }
 
-//head([5,6,7]);
+
   
-// TEST CODE
-assertEqual("Lighthouse Labs", "Bootcamp");
-assertEqual(1, 1);
-assertEqual("Lighthouse Labs", "Lighthouse Labs");
-assertEqual(1, 10);
-assertEqual(head([5,6,7]), 5);
-assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
