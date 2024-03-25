061f0f3875fddb41479bd9f70a50c1e920d6d2aa
ZiquitaRiberdy <riberdyziquita@gmail.com>
2024-03-25 10:31:26 -0700

    added test cases

--- takeUntil.js
+++ takeUntil.js
@@ -1,3 +1,30 @@
+const assertArraysEqual = function(ar1, ar2) {
+    //eqArrays(ar1,ar2);
+    if (eqArrays(ar1,ar2)) {
+      console.log(`✅✅✅ Assertion Passed: ${ar1} === ${ar2}`) 
+    } else {
+        console.log(`🛑🛑🛑 Assertion Failed: ${ar1} !== ${ar2}`)
+    }
+  };
+
+const eqArrays = function(arr1, arr2){
+    let answer = true;
+
+    if (arr1.length !== arr2.length){
+        answer = false;
+    } else {
+        //return answer;
+        for (let i = 0; i < arr1.length; i++){
+            if (arr1[i] !== arr2[i]) {
+                answer = false;
+                break;
+            }
+        }
+    }
+    return answer;
+    //assertArraysEqual(answer);
+  }
+
 const takeUntil = function(array, callback) {
     // ...
     for (let i = 0; i <array.length; i++) {
