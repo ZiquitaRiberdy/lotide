3fd759b0c68596be5b31a0b31e8d981cbedc1fc9
ZiquitaRiberdy <riberdyziquita@gmail.com>
2024-03-25 09:51:53 -0700

    changed testcase to include eqarrays

--- map.js
+++ map.js
@@ -1,11 +1,29 @@
 const assertArraysEqual = function(ar1, ar2) {
     //eqArrays(ar1,ar2);
-    if (ar1,ar2) {
+    if (eqArrays(ar1,ar2)) {
       console.log(`✅✅✅ Assertion Passed: ${ar1} === ${ar2}`) 
     } else {
         console.log(`🛑🛑🛑 Assertion Failed: ${ar1} !== ${ar2}`)
     }
   };
+
+const eqArrays = function(arr1, arr2){
+    let answer = true;
+
+    if (arr1.length !== arr2.length){
+        answer = false;
+    } else {
+        //return answer;
+        for (let i = 0; i < arr1.length; i++){
+            if (arr1[i] !== arr2[i]) {
+                answer = false;
+                break;
+            }
+        }
+    }
+    return answer;
+    //assertArraysEqual(answer);
+  }
 
 const words = ["ground", "control", "to", "major", "tom"];
 
@@ -18,4 +36,4 @@ const map = function(array, callback) {
 }
 const results1 = map(words, (word) => word[0]);
 //(word) => word[0] is the anonymous function 
-assertArraysEqual(results1, true);
+assertArraysEqual(results1, [g,c,t,m,t]);
