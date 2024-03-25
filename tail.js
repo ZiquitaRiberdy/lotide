da377f54f25a1bc9ebfd1eda76f2723e9b214403
ZiquitaRiberdy <riberdyziquita@gmail.com>
2024-03-25 13:55:24 -0700

    added test file for tail

--- tail.js
+++ tail.js
@@ -1,10 +1,4 @@
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
+const assertEqual = require("./assertEqual");
 
 const tail = (tailArray) => {
   return tailArray.slice(1).length;
@@ -12,6 +6,4 @@ const tail = (tailArray) => {
 
 const words = ["Yo Yo", "Lighthouse", "Labs"];
 tail(words); // no need to capture the return value since we are not checking it
-assertEqual(words.length, 3);
-assertEqual(tail(words),2);
-assertEqual(tail(words),10);
+
---
+++ test/tailTest.js
@@ -0,0 +1,3 @@
+assertEqual(words.length, 3);
+assertEqual(tail(words),2);
+assertEqual(tail(words),10);
